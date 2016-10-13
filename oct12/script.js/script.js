926257bd94ae5939bfe9b52a7714bb484ea00305
Elvis Garcia Jimenez <6003367@kjuhsd.k12.ca.us>
2016-10-13 15:51:32 -0700

    oct13

---
+++ oct12/script.js/script.js
@@ -0,0 +1,18 @@
+var span = document.querySelectorAll('legend span');
+var input = document.querySelectorAll('legend input');
+ //console.log(span);
+ console.log(input);
+ for(var i = 0; i < input.length; i++) {
+   input[i].addEventListener('click', function(){
+     //console.log(this)
+    // this.classList.toggle('placeholder');
+    console.log(this === document.activeElement);
+    if(this === document.activeElement){
+      this.previousElementSibling.classList.toggle.add('placeholder');
+  }else {
+    //this.previousElementSibling.classList.toggle('placeholder');
+    for(var j = 0; j < span.length; j++)
+    span[j].classList.remove('placeholder')
+  }
+   });
+ }
